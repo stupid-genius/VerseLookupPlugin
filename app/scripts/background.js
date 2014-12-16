272b844b2ed0543353257c3c9b9dec43e35774da
Allen Ng <ang@yieldex.com>
2014-12-16 10:31:58 -0700

    Basic tooltip working; grunt build runs TO DO: styling support nultiple refs per node support .on()

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,7 +1,21 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+var versePattern = /((?:[Ii\d]\s+)?\w{3,})\s+(\d{1,3})\s*:\s*(\d{1,3}(?:-\d{1,3})?)/g;
+
+chrome.runtime.onInstalled.addListener(function(details) {
+	console.log('previousVersion', details.previousVersion);
 });
 
-console.log('\'Allo \'Allo! Event Page');
+chrome.omnibox.onInputEntered.addListener(function(input, disposition){
+	disposition = 'newForegroundTab';
+	if(input.match(versePattern)){
+		$.ajax({
+			url: input.replace(versePattern, 'https://labs.bible.org/api/?passage=$1+$2:$3')
+		})
+		.done(function(verse){
+			console.log(verse);
+			console.log('omnibox command: %s', verse);
+		});
+	}
+});
+
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,33 +1,43 @@
 'use strict';
 
-var versePattern = /((?:[Ii\d]\s+)?\w{3,})\s+(\d{1,3})\s*:\s*(\d{1,3})/g;
+var versePattern = /((?:[Ii\d]\s+)?\w{3,})\s+(\d{1,3})\s*:\s*(\d{1,3}(?:-\d{1,3})?)/g;
 
 $(document).ready(function(){
 	console.log('content script running');
-	var count = 0;
+
+	/*
+	need to add tests
+	support more than one ref per node
+	create options page
+	*/
+
+	var count = 1;
 	$('body *:not("script")').contents().filter(function(){
 		return this.nodeType == Node.TEXT_NODE; //3
 	}).each(function(){
 		var elem = $(this);
+		var parent = elem.parent();
 		var txt = elem.text();
-		if(txt!=undefined && !txt.match(/^\s*$/)){
+		if(txt!==undefined && !txt.match(/^\s*$/)){
 			if(txt.match(versePattern)){
-				//console.log('%d. Text: %s\nPosition:%o\nOffset:%o', ++count, txt, elem.parent().position(), elem.parent().offset());
-				var refs = txt.match(versePattern)[0];
-				// validation
-				//console.log(refs.replace(versePattern, 'https://labs.bible.org/api/?passage=$1+$2:$3'));
-				// perform lookup
-				$.ajax({
-					url: refs.replace(versePattern, 'https://labs.bible.org/api/?passage=$1+$2:$3')
-				})
-				.done(function(text){
-					// build tooltip
-					var verseId = 'verse'+ ++ count;
-					var inject = '<span id="'+ verseId + '">' + refs + '</span>';
-					elem.parent().html(elem.parent().html().replace(refs, inject));
-					// create tooltip
-					Tipped.create('#'+verseId, text);
-				});
+				var refs = txt.match(versePattern);
+				for(var i in refs){
+					console.log(refs[i]);
+					// perform lookup
+					$.ajax({
+						url: refs[i].replace(versePattern, 'https://labs.bible.org/api/?passage=$1+$2:$3')
+					})
+					.done(function(text){
+						// build tooltip
+						var verseId = 'verse'+count++;
+						var inject = '<span id="'+ verseId + '">' + refs[i] + '</span>';
+						console.log(text);
+						parent.html(parent.html().replace(refs[i], inject));
+						// create tooltip
+						console.log(elem.get(0));
+						Tipped.create('#'+verseId, text);
+					});
+				}
 			}
 		}
 	});
